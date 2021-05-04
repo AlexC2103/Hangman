47c76d7d72a39699634bade8b1d26ef75643105c
AlexC2103 <80043678+AlexC2103@users.noreply.github.com>
2021-05-04 12:08:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -29,16 +29,12 @@ function startGame() {
   input.setAttribute('placeholder', 'Game already active');
   input.disabled = true;
   submit.disabled = true;
-  for (var i = 0; i < word.length; ++i) {
-    if (i === 0) {
-      document.getElementById('word').innerHTML += word[i];
-    } else
-    if (i === word.length - 1) {
-      document.getElementById('word').innerHTML += word[word.length - 1];
-    } else {
-      document.getElementById('word').innerHTML += '_';
-    }
+  document.getElementById('word').innerHTML += word[0];
+  for (var i = 1; i < word.length - 1; ++i) {
+    document.getElementById('word').innerHTML += '_';
   }
+
+  document.getElementById('word').innerHTML += word[word.length - 1];
 
   returnWord = word;
 
@@ -51,32 +47,32 @@ function startGame() {
 
 var mistakes = 0;
 
-function checkLetter(id) {
+function checkLetter(clickedButtonId) {
   gameCheck();
   var ok = 0;
-  if (word.indexOf(id) !== -1) {
+  if (word.indexOf(clickedButtonId) !== -1) {
     for (var i = 1; i < word.length - 1; ++i) {
-      if (id === word[i]) {
-        returnWord = setCharAt(returnWord, i, id);
+      if (clickedButtonId === word[i]) {
+        returnWord = setCharAt(returnWord, i, clickedButtonId);
         ok = 1;
-        if (id === word[0]) {
-          returnWord = setCharAt(returnWord, 0, id);
+        if (clickedButtonId === word[0]) {
+          returnWord = setCharAt(returnWord, 0, clickedButtonId);
         }
       }
     }
 
-    document.getElementById(id).style.color = '#00ff0a';
-    document.getElementById(id).disabled = 'true';
+    document.getElementById(clickedButtonId).style.color = '#00ff0a';
+    document.getElementById(clickedButtonId).disabled = 'true';
   } else {
     mistakes++;
-    document.getElementById(id).style.color = '#ff0000';
-    document.getElementById(id).disabled = 'true';
-    document.getElementById('mistakesNumber').innerHTML = 'Number of mistakes: ' + mistakes;
+    document.getElementById(clickedButtonId).style.color = '#ff0000';
+    document.getElementById(clickedButtonId).disabled = 'true';
+    document.getElementById('mistakesNumber').innerHTML = 'Number of mistakes: ' + mistakes + ' / 6';
   }
 
   if (ok) {
-    document.getElementById(id).style.color = '#00ff0a';
-    document.getElementById(id).disabled = 'true';
+    document.getElementById(clickedButtonId).style.color = '#00ff0a';
+    document.getElementById(clickedButtonId).disabled = 'true';
   }
 
   document.getElementById('word').innerHTML = returnWord;
